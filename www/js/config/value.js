46b60a4f8556b87e9c1e313d6682169c76e7153d
wei2lee <wei2lee86@gmail.com>
2015-11-05 10:36:21 +0800

    Change to new ip for dev

--- www/js/config/value.js
+++ www/js/config/value.js
@@ -6,8 +6,8 @@
         minimumCheckAppVersionTimeSpan:24*3600
     }).value('APIService', {
         token:{},
-        apiBase:'http://103.9.149.59:8034/',
-        resourceBase:'http://103.9.149.59:8034/data/HATTEN/',
+        apiBase:'http://103.9.149.59:6080/',
+        resourceBase:'http://103.9.149.59:6080/data/HATTEN/',
         apiUserName:'90731C01@hatt',
         apiUserPassword:'F2568907B18C'
     });
